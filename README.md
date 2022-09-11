363d136d3153aaeac6b076a348af04a60661c6e9
levosay <ryuuzaki44l@gmail.com>
2022-09-11 14:44:19 +0300

    added: README

---
+++ README.md
@@ -0,0 +1,21 @@
+# О проекте
+Чат для ознакомления с возможностями WebSocket 
+
+[Node.js](https://nodejs.org/en/download/) версии 14 и выше.
+## Client
+
+Использовался сборщик [Vite](https://vitejs.dev/). Проект написан при помощи библиотеки [React](https://ru.reactjs.org/) 18.2.0
+
+npm install
+
+npm run dev
+
+Проект запускает по:  http://127.0.0.1:5173/
+
+## Server
+
+npm install
+
+npm run server
+
+Проет стартует на 5000 порту
--- client/src/components/ChatMassages/ChatMassages.jsx
+++ client/src/components/ChatMassages/ChatMassages.jsx
@@ -1,7 +1,8 @@
 import { Message } from '@components'
 import classes from './ChatMassages.module.sass'
+import {useEffect} from 'react'
 
-export const ChatMassages = ({massages}) => {
+export const ChatMassages = ({ massages }) => {
   const massageList = massages &&
     massages.map(mess =>
       <Message
@@ -11,11 +12,21 @@ export const ChatMassages = ({massages}) => {
       />
     )
 
-  console.log('massages ', massages)
+  const scrollDown = () => {
+    const scrollBlock = document.querySelector('#scroll-block')
+    scrollBlock.scrollTop = scrollBlock.scrollHeight
+  }
+
+  useEffect(() => {
+    scrollDown()
+  }, [massages])
 
   return (
-    <div className={classes.wrapper}>
-      {massageList}
+    <div className={classes.wrapperScroll}>
+      <div id="scroll-block" className={classes.wrapper}>
+        {massageList}
+      </div>
     </div>
+
   )
 }
